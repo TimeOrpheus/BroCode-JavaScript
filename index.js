7aeee4be9025505c52a241c81a7d746d11ad2d0b
TimeOrpheus <anikthe1@outlook.com>
2023-04-12 17:51:33 +0600

    JaveScript Prctice

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("I like Pizza!");
+console.log("It's really good!");
+//This is single line comment
+/* And this is
+multi line comment*/
+
+let age = 21;
+let firstName = "Bro";
+let student = true;
+
+console.log("Hello", firstName);
+console.log("You are", age, "years old");
+console.log("Enrolled", student);
+
+
+document.getElementById("p1").innerHTML = "Hello" + firstName;
+document.getElementById("p2").innerHTML = "You are" + age + "years old";
+document.getElementById("p3").innerHTML = "Enrolled:" + student; 
